1948131b5f9a887618423a4d573296595d617b63
itzsriharishetty-commits <itzsriharishetty@gmail.com>
2026-09-01 09:30:29 +0530

    Update backend URL to production server

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'http://localhost:8080';
+const BACKEND_URL = 'https://price-compare-backend.onrender.com';
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.type === 'INGEST') {
